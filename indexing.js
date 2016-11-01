71bcf70fc026789a96ad05d49fc85564b414872c
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-11-01 16:27:52 +0100

    Fixed the indexing and upgraded to current collections-online/master

--- indexing.js
+++ indexing.js
@@ -1,5 +1,14 @@
 'use strict';
 
+var co = require('collections-online');
+
+// This allows loading of environment variables from a .env file
+require('dotenv').config({silent: true});
+// Loading the configuration
+var state = {};
+var config = require('./config');
+co.config(config);
+
 // Configure collections-online
 var config = require('./config');
 require('collections-online').config(config);
@@ -7,4 +16,4 @@ require('collections-online').config(config);
 // This registers the cumulus indexing-engine
 require('collections-online-cumulus').registerPlugins();
 // Start the indexing
-require('collections-online/indexing');
+require('collections-online/indexing').run();
