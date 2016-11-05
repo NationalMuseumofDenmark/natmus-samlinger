e8bd37e0c730ab5a03f371cca6d6affc91c3518c
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-11-05 22:51:04 +0100

    Changed the list of all indecies

--- config/env/development.js
+++ config/env/development.js
@@ -7,15 +7,9 @@ module.exports = _.merge(base, {
   env: 'development',
   es: {
     allIndecies: [
-      'assets',
-      'dev_mom_objects_public',
-      'dev_es_objects_public',
-      'dev_as_objects_public',
-      'dev_fhm_objects_public',
-      'dev_dnt_objects_public',
-      'dev_kmm_objects_public',
-      'dev_mus_objects_public',
-      'dev_flm_objects_public'
+      'assets'
+      //'new_assets',
+      //'new_objects'
     ]
   },
   googleAnalyticsPropertyID: 'no-please',
