4740287616c9b6ae12a0e01279758c7308cc15c9
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-11-06 13:15:47 +0100

    First take on a direct access with the natmus API

--- controllers/types/object.js
+++ controllers/types/object.js
@@ -6,11 +6,46 @@ const config = require('collections-online/lib/config');
 const section = require('collections-online/lib/section');
 const objectSection = section('object');
 
+const Q = require('q');
+const request = require('request');
+const querystring = require('querystring');
+
+function getObject(req) {
+  let deferred = Q.defer();
+  let id = req.params.id;
+  let collection = req.params.collection;
+
+  let query = {
+    sourceId: id,
+    collection
+  };
+  let queryString = Object.keys(query).map((key) => {
+    return key + ':' + query[key];
+  }).join(' AND ');
+
+  let qs = querystring.stringify({
+    query: queryString,
+    size: 1
+  });
+
+  let url = 'http://testapi.natmus.dk/search/public/elasticSearchString?' + qs;
+  request({
+    url
+  }, (err, res) => {
+    if(err) {
+      deferred.reject(err);
+    } else {
+      deferred.resolve(res);
+    }
+  });
+  return deferred.promise;
+}
+
 /**
  * Renders an asset's landing page
  */
 exports.index = function(req, res, next) {
-  return documentCtrl.get(req, 'object').then((metadata) => {
+  return getObject(req).then((metadata) => {
     return {
       'metadata': metadata,
       'objectSection': objectSection({}),
