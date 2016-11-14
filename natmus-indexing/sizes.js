b4773dca469f5c9d0e4adb14ef6c4e8a609c1748
Andreas Larsen <andreas@larsenwork.com>
2016-11-14 23:16:55 +0100

    Fix asset technical info layout

--- natmus-indexing/sizes.js
+++ natmus-indexing/sizes.js
@@ -1,5 +1,13 @@
 module.exports = (metadata) => {
+  // Remap for easier backwards compability + dots within asset.json
+  // doesn't seem to work
   metadata.width_px = metadata.file.dimensions.width;
   metadata.height_px = metadata.file.dimensions.height;
+
+  // Cleanup
+  delete metadata.file.dimensions.width;
+  delete metadata.file.dimensions.height;
+
+  // Return
   return metadata;
 };
