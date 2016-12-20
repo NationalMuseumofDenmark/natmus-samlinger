5727cdc9aab546cc51361f64f74ba999b578f367
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-12-20 15:42:15 +0100

    Enabled redirect for /search on ?catalog

--- controllers/search.js
+++ controllers/search.js
@@ -5,9 +5,20 @@ const querystring = require('querystring');
 const searchController = require('collections-online/lib/controllers/search');
 
 module.exports.redirect = function(req, res, next) {
-  const catalog = req.params[0];
-  // Replace a path param on catalog, with a query param
-  req.query.collection = catalog;
-  var qs = querystring.stringify(req.query);
-  res.redirect('/' + config.search.path + '?' + qs);
+  if(req.params.length) {
+    const catalog = req.params[0];
+    // Replace a path param on catalog, with a query param
+    req.query.collection = catalog;
+    var qs = querystring.stringify(req.query);
+    res.redirect('/' + config.search.path + '?' + qs);
+  } else if(req.query.catalog) {
+    const catalog = req.query.catalog;
+    delete req.query.catalog;
+    // Replace a path param on catalog, with a query param
+    req.query.collection = catalog;
+    var qs = querystring.stringify(req.query);
+    res.redirect(req.path + '?' + qs);
+  } else {
+    next();
+  }
 };
--- routers/main.js
+++ routers/main.js
@@ -1,6 +1,7 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const config = require('../config');
 
 const searchController = require('../controllers/search');
 
@@ -8,5 +9,9 @@ router.use('/api', require('./api'));
 
 // Requesting a bare catalog (/ES, /DNT, ...) redirects to a search
 router.get(/^\/([A-Z]{2,3})$/, searchController.redirect);
+router.get(/^\/([A-Z]{2,3})$/, searchController.redirect);
+
+const searchPath = encodeURIComponent(config.search.path);
+router.get('/' + searchPath, searchController.redirect);
 
 module.exports = router;
