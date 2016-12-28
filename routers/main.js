2e427f636d0198d50c6c905ee26774b3ba22460e
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-12-28 13:22:16 +0100

    Redirect assets from their old route

--- routers/main.js
+++ routers/main.js
@@ -10,6 +10,12 @@ router.use('/api', require('./api'));
 // Requesting a bare catalog (/ES, /DNT, ...) redirects to a search
 router.get(/^\/([A-Z]{2,3})$/, searchController.redirect);
 router.get(/^\/([A-Z]{2,3})$/, searchController.redirect);
+// Redirecting assets
+router.get(/^\/([A-Z]{2,3})\/(\d+)$/, (req, res) => {
+  let catalog = req.params[0];
+  let id = req.params[1];
+  res.redirect('/' + catalog + '/asset/' + id);
+});
 
 const searchPath = encodeURIComponent(config.search.path);
 router.get('/' + searchPath, searchController.redirect);
