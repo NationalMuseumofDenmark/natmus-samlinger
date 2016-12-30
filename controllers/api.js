0a015b4aab24ad14222b0dcf9a42aab998de26be
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-12-30 13:55:44 +0100

    Added a /api documentation route

--- controllers/api.js
+++ controllers/api.js
@@ -2,29 +2,36 @@
 
 const natmus = require('../services/natmus-api');
 
-module.exports.proxy = (req, res, next) => {
-  let index = req.params.index || 'all';
-  let action = req.params.action;
+const api = {
+  documentation: (req, res) => {
+    res.redirect('https://docs.google.com/document/d/1LsFv5_iWbjoQU8oz-Y7dit5dyyeHKZR0ixvRkcrolmM/edit');
+  },
+  proxy: (req, res, next) => {
+    let index = req.params.index || 'all';
+    let action = req.params.action;
 
-  if(index !== 'all') {
-    throw new Error('Unsupported index: ' + index);
-  }
+    if(index !== 'all') {
+      throw new Error('Unsupported index: ' + index);
+    }
 
-  if(action !== '_search') {
-    throw new Error('Unsupported action: ' + action);
-  }
+    if(action !== '_search') {
+      throw new Error('Unsupported action: ' + action);
+    }
 
-  let body = req.body || {};
-  if(req.query.size) {
-    body.size = req.query.size;
-  }
-  if(req.query.from) {
-    body.from = req.query.from;
-  }
+    let body = req.body || {};
+    if(req.query.size) {
+      body.size = req.query.size;
+    }
+    if(req.query.from) {
+      body.from = req.query.from;
+    }
 
-  natmus.search({
-    body
-  }).then((response) => {
-    res.json(response);
-  }, next);
+    natmus.search({
+      body
+    }).then((response) => {
+      res.json(response);
+    }, next);
+  }
 };
+
+module.exports = api;
--- routers/api.js
+++ routers/api.js
@@ -5,6 +5,9 @@ const router = express.Router();
 const api = require('../controllers/api');
 
 router
+  .get('/', api.documentation);
+
+router
   .post('/:action', api.proxy)
   .post('/:index/:action', api.proxy);
 
