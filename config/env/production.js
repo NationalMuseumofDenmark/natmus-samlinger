558ad8c2894a4d4a93633f70741885259a0b447f
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-12-30 13:57:57 +0100

    Using testapi on beta and api in production

--- config/env/production.js
+++ config/env/production.js
@@ -7,7 +7,11 @@ module.exports = _.merge(base, {
   enableGeotagging: true,
   env: 'production',
   googleAnalyticsPropertyID: 'UA-2930791-3',
-  natmusApiBaseURL: 'http://api.natmus.dk/',
+  natmus: {
+    api: {
+      baseURL: 'http://api.natmus.dk'
+    }
+  },
   siteTitle: 'Nationalmuseets Samlinger Online',
   viewsPath: '/views',
 });
--- config/env/test.js
+++ config/env/test.js
@@ -8,7 +8,11 @@ module.exports = _.merge(base, {
   env: 'test',
   googleAnalyticsPropertyID: 'UA-2930791-7',
   ip: process.env.IP || '0.0.0.0',
-  natmusApiBaseURL: 'http://testapi.natmus.dk/',
+  natmus: {
+    api: {
+      baseURL: 'http://testapi.natmus.dk'
+    }
+  },
   port: process.env.PORT || 9000,
   siteTitle: 'Nationalmuseets Samlinger Online (beta)',
   viewsPath: '/views',
