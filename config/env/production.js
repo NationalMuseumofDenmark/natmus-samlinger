8b7bac3c43efa73f37e7d84bfa4b32cce4297b08
syntax42 <syntax42@gmail.com>
2018-08-22 08:51:43 +0200

    Using api.natmus.dk in prod

--- config/env/production.js
+++ config/env/production.js
@@ -11,7 +11,7 @@ module.exports = _.merge(base, {
   host: 'samlinger.natmus.dk',
   natmus: {
     api: {
-      baseURL: 'http://testapi.natmus.dk'
+      baseURL: 'http://api.natmus.dk'
     }
   },
   siteTitle: 'Nationalmuseets Samlinger Online',
